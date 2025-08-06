f31d78804c3b878ca92c06bd0fc8e02239bd2418
rasult22 <warrior.006@inbox.ru>
2025-08-06 20:34:05 +0500

    fix: remove test reset button from production code
    The reset button was only meant for testing purposes and should not be visible in production. This commit removes both the HTML and React component implementations.

--- src/App.tsx
+++ src/App.tsx
@@ -203,12 +203,12 @@ function App() {
             </div>
             
             {/* Reset button for testing - remove in production */}
-            <button
+            {/* <button
               onClick={resetForm}
               className="mt-4 text-sm text-gray-500 hover:text-gray-700 underline"
             >
               Изменить ответ (для тестирования)
-            </button>
+            </button> */}
           </div>
         )}
       </div>
